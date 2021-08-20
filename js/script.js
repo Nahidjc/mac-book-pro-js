f577d2182842c06b19cccb7bf35eea4aa6d1574e
Nahidjc <nahidjc80@gmail.com>
2021-08-21 00:40:18 +0600

    cost adding

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function storageCost(cost) {
+    console.log(cost);
+    document.getElementById('storage-cost').innerHTML = cost;
+}
+
+function memoryCost(cost) {
+    console.log(cost);
+    document.getElementById('memory-cost').innerHTML = cost;
+}
+
+function deliveryCost(cost) {
+    document.getElementById('delivery-charge').innerHTML = cost;
+}
